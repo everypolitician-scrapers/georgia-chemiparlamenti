3637df23d0ef410b922d168d8804a20ce37d0912
Tony Bowden <tony@tmtm.com>
2017-04-27 17:12:41 +0100

    Rewrite for new term

--- scraper.rb
+++ scraper.rb
@@ -10,45 +10,77 @@ require 'scraperwiki'
 # OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
+require 'scraped'
 
-def scrape_list(url)
-  noko = noko_for(url)
-  noko.css('li.member a/@href').map(&:text).uniq.each do |link|
-    scrape_member URI.join(url, link)
+class MembersPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::CleanUrls
+
+  field :member_urls do
+    noko.xpath('//a[@data-mp-id]/@href').map(&:text).uniq
   end
 end
 
-def scrape_member(url)
-  noko = noko_for(url)
+class MemberPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::CleanUrls
 
-  box = noko.css('div#representative')
-  links = box.xpath('.//td[contains(.,"Links")]/following-sibling::td')
-  contacts = box.xpath('.//td[contains(.,"Contact Address")]/following-sibling::td').text.lines.map(&:tidy).reject(&:empty?)
-  phone = contacts.shift.to_s.sub(/^- /, '')
-  address = contacts.pop
-  email = contacts.first.to_s.sub(/^- /, '')
+  field :id do
+    url.split('/').last
+  end
 
-  data = {
-    id:        url.to_s[/representative\/(\d+)/, 1],
-    name:      box.css('div#name').text.tidy,
-    image:     box.css('div#photo img/@src').text,
-    district:  box.xpath('.//td[contains(.,"Electoral District")]/following-sibling::td').text,
-    elected:   box.xpath('.//td[contains(.,"Elected Date")]/following-sibling::td').text,
-    party:     box.xpath('.//td[contains(.,"Faction")]/following-sibling::td').text,
-    website:   links.css('a[href*="parliament.ge"]/@href').text,
-    wikipedia: links.css('a[href*="wikipedia.org"]/@href').text,
-    facebook:  links.css('a[href*="facebook.com"]/@href').text,
-    twitter:   links.css('a[href*="twitter.com"]/@href').text,
-    phone:     phone,
-    email:     email,
-    term:      8,
-    source:    url.to_s,
-  }
-  data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
-  ScraperWiki.save_sqlite(%i(id term), data)
+  field :name do
+    box.css('h4').text.tidy
+  end
+
+  field :birth_date do
+    noko.xpath('//h3[.="Birth Date / Place"]/following::p').map(&:text).map(&:tidy).first[/(\d{4}-\d{2}-\d{2})/, 1]
+  end
+
+  field :email do
+    noko.css('img[src*="sms.png"] + p').text.tidy
+  end
+
+  field :party do
+    box.css('h4 + p').text.tidy
+  end
+
+  field :start_date do
+    mandate.first
+  end
+
+  field :end_date do
+    return '' if mandate.last == 'Current'
+    mandate.last
+  end
+
+  field :area do
+    noko.xpath('//h3[.="Electoral District"]/following::p').map(&:text).map(&:tidy).first
+  end
+
+  field :area_id do
+    area.to_s[/#(\d+)/, 1]
+  end
+
+  private
+
+  def box
+    noko.css('div.mp-columns')
+  end
+
+  def mandate
+    noko.at_xpath('//h3[.="Date of Terminition of Mandate"]/following::p').text.tidy
+        .gsub('Current', '').split('- ').map(&:tidy)
+  end
 end
 
-scrape_list('http://www.chemiparlamenti.ge/en/who/unit/parliament/')
+def scraper(h)
+  url, klass = h.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
+
+start = 'http://www.chemiparlamenti.ge/en/parliamentarians'
+data = scraper(start => MembersPage).member_urls.map do |url|
+  scraper(url => MemberPage).to_h.merge(term: 9)
+end
+
+# data.each { |r| puts r.reject { |k,v| v.to_s.empty? }.sort_by { |k,v| k }.to_h }
+ScraperWiki.save_sqlite(%i[id term], data)
