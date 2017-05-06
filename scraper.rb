baef766395afcb72b0e5f4f258227ec423128d72
Tony Bowden <tony@tmtm.com>
2017-05-06 07:28:19 +0100

    WIP: Factor out 'Scraper' and 'ScraperRun' classes

--- scraper.rb
+++ scraper.rb
@@ -3,26 +3,63 @@
 # frozen_string_literal: true
 
 require 'pry'
-require 'scraped'
-require 'scraperwiki'
+require 'require_all'
 
 require_rel 'lib'
 
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
-require 'scraped_page_archive/open-uri'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+# require 'scraped_page_archive/open-uri'
 
-def scraper(h)
-  url, klass = h.to_a.first
-  klass.new(response: Scraped::Request.new(url: url).response)
+class Scraper
+  require 'scraped'
+
+  def initialize(h)
+    args = h.to_a
+    @url, @klass = args.shift
+    @conf = args.to_h
+  end
+
+  def scraped
+    @scraped ||= klass.new(response: Scraped::Request.new(url: url).response)
+  end
+
+  def data
+    defaults.merge(scraped.to_h)
+  end
+
+  private
+
+  attr_reader :url, :klass, :conf
+
+  def defaults
+    @conf[:defaults] || {}
+  end
+end
+
+class ScraperRun
+  require 'scraperwiki'
+
+  def initialize(table: 'data', unique_by: 'id', debug: ENV['MORPH_DEBUG'])
+    @table = table
+    @unique_by = unique_by
+    @debug = debug
+  end
+
+  def save(data)
+    data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if debug
+    ScraperWiki.sqliteexecute('DROP TABLE %s' % table) rescue nil
+    ScraperWiki.save_sqlite(unique_by, data)
+  end
+
+  private
+
+  attr_reader :table, :unique_by, :debug
 end
 
 start = 'http://www.chemiparlamenti.ge/en/parliamentarians'
-data = scraper(start => MembersPage).member_urls.map do |url|
-  scraper(url => MemberPage).to_h.merge(term: 9)
+data = Scraper.new(start => MembersPage).scraped.member_urls.map do |url|
+  Scraper.new(url => MemberPage, :defaults => { term: 9 }).data
 end
 
-data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
-
-ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-ScraperWiki.save_sqlite(%i[id term], data)
+ScraperRun.new(table: 'data', unique_by: %i[id term]).save(data)
