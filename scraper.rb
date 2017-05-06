b2d42d7d50ac9dfa195806ae98110ce4a19575e0
Tony Bowden <tony@tmtm.com>
2017-05-06 08:16:23 +0100

    WIP

--- scraper.rb
+++ scraper.rb
@@ -11,55 +11,85 @@ require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 # require 'scraped_page_archive/open-uri'
 
-class Scraper
-  require 'scraped'
+module EveryPolitician
+  class ScrapedPage
+    require 'scraped'
 
-  def initialize(h)
-    args = h.to_a
-    @url, @klass = args.shift
-    @conf = args.to_h
+    def initialize(h)
+      args = h.to_a
+      @url, @klass = args.shift
+      @conf = args.to_h
+    end
+
+    def scraped
+      @scraped ||= klass.new(response: Scraped::Request.new(url: url).response)
+    end
+
+    def data
+      scraped.to_h
+    end
+
+    private
+
+    attr_reader :url, :klass, :conf
   end
 
-  def scraped
-    @scraped ||= klass.new(response: Scraped::Request.new(url: url).response)
+  class ScraperRun
+    require 'scraperwiki'
+
+    # TODO: add a runid (default to UUID? Or auto-inc?) + start_time
+    def initialize(table: 'data', unique_by: 'id', debug: false)
+      @table = table
+      @unique_by = unique_by
+      @debug = debug
+    end
+
+    def save(rows)
+      rows.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if debug
+      ScraperWiki.sqliteexecute('DROP TABLE %s' % table) rescue nil
+      ScraperWiki.save_sqlite(unique_by, rows)
+      # TODO: save run data (start / end / rowcount)
+    end
+
+    private
+
+    attr_reader :table, :unique_by, :debug
   end
 
-  def data
-    defaults.merge(scraped.to_h)
-  end
+  # A series of classes for standardised approaches
+  #   - list page + individual members
+  #   - all on one page, with fragments, etc
+  # We can ship some basics, and expect many to be subclassed
+  class Scraper
+    class MemberList
+      def initialize(list_url:, list_class:, member_class:, defaults: {})
+        @list_url = list_url
+        @list_class = list_class
+        @member_class = member_class
+        @defaults = defaults
+      end
 
-  private
+      def data
+        # TODO: default approach should be a `members` method, not `member_urls`
+        # then we merge them together based on the `source` field
+        ScrapedPage.new(list_url => list_class).scraped.member_urls.map do |member_url|
+          defaults.merge(ScrapedPage.new(member_url => member_class).data)
+        end
+      end
 
-  attr_reader :url, :klass, :conf
+      private
 
-  def defaults
-    @conf[:defaults] || {}
+      attr_reader :list_url, :list_class, :member_class, :defaults
+    end
   end
 end
 
-class ScraperRun
-  require 'scraperwiki'
 
-  def initialize(table: 'data', unique_by: 'id', debug: false)
-    @table = table
-    @unique_by = unique_by
-    @debug = debug
-  end
-
-  def save(rows)
-    rows.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if debug
-    ScraperWiki.sqliteexecute('DROP TABLE %s' % table) rescue nil
-    ScraperWiki.save_sqlite(unique_by, rows)
-  end
-
-  private
-
-  attr_reader :table, :unique_by, :debug
-end
-
-start = 'http://www.chemiparlamenti.ge/en/parliamentarians'
-data = Scraper.new(start => MembersPage).scraped.member_urls.map do |url|
-  Scraper.new(url => MemberPage, defaults: { term: 9 }).data
-end
-
-ScraperRun.new(unique_by: %i[id term], debug: ENV['MORPH_DEBUG']).save(data)
+EveryPolitician::ScraperRun.new(unique_by: %i[id term], debug: ENV['MORPH_DEBUG']).save(
+  EveryPolitician::Scraper::MemberList.new(
+    list_url: 'http://www.chemiparlamenti.ge/en/parliamentarians',
+    list_class: MembersPage,
+    member_class: MemberPage,
+    defaults: { term: 9 },
+  ).data
+)
